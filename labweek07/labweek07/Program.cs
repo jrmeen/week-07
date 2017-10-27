6cd1b38491204964240b0a9e0d4153a267a7a637
meen <59030143@kmitl.ac.th>
2017-10-28 00:03:55 +0700

    w07p08

--- labweek07/labweek07/Program.cs
+++ labweek07/labweek07/Program.cs
@@ -11,11 +11,16 @@ namespace labweek07
         static void Main(string[] args)
         {
             Teacher teacher = new Teacher("Tom", 350f);
-            // teacher work for 20Hr/month
+            Professor prof = new Professor("Jerry", 500f, 5000f);
+            // teacher work 20Hr/month
             Console.WriteLine("{0} charge = {1}", teacher.TypeName(),
                 teacher.CalculateCharge(20f));
+            // professor work 12Hr/month
+            Console.WriteLine("{0} charge = {1}", prof.TypeName(),
+                prof.CalculateCharge(12F));
             Console.ReadLine();
         }
+
 
     }
 
@@ -41,7 +46,34 @@ namespace labweek07
         protected float billingRate;
     }
 
-    
+    class Professor : Teacher
+    {
+        private float emolument;  // เงินประจำตำแหน่ง
+
+        public Professor(string name, float billingRate) : base(name, billingRate)
+        {
+        }
+
+        public Professor(string name, float billingRate, float emolument)
+        : this(name, billingRate)
+        {
+            this.emolument = emolument;
+        }
+
+        // new function, because it's different than the base version
+        public new float CalculateCharge(float hours)
+        {
+            if (hours < 1.0F)
+                hours = 1.0F; // minimum charge.
+            return (hours * billingRate) + emolument;
+        }
+        // new function, because it's different than the base version
+        public new string TypeName()
+        {
+            return ("Professor");
+        }
+    }
+
 
 
 
