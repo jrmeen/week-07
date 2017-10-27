a33a8376d63911dc4268cc9b0ca18203d00d2ac6
meen <59030143@kmitl.ac.th>
2017-10-27 23:50:42 +0700

    uncomment

--- labweek07/labweek07/Program.cs
+++ labweek07/labweek07/Program.cs
@@ -13,7 +13,7 @@ namespace labweek07
             Student su = new Student("Student Name", "12345678");
             try
             {
-                // su.Name = "My name";
+                 su.Name = "My name";
                 su.GPA = 3.5f;
                 Console.WriteLine("Student name : " + su.Name);
                 Console.WriteLine("Student ID   : " + su.ID);
