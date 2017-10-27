348021f0c6aee40ffd3d86ed0fe898e2caa84ca1
meen <59030143@kmitl.ac.th>
2017-10-27 23:35:54 +0700

    w07p01

--- labweek07/labweek07/Program.cs
+++ labweek07/labweek07/Program.cs
@@ -22,9 +22,9 @@ namespace labweek07
     }
     class Student
     {
-        public string Name;
-        public string ID;
-        public float GPA;
+        private string Name;
+        private string ID;
+        private float GPA;
     }
 
 }
